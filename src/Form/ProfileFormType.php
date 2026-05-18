eb71bf97fb1894b5859c1375356a5156b19fcbae
Dravel IMBOLO <dravelameguste@gmail.com>
2026-05-18 07:14:41 +0200

    feat(profile): add ProfileFormType and ChangePasswordFormType #7

---
+++ src/Form/ProfileFormType.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form;
+
+use App\DTO\UpdateProfileDTO;
+use App\Enum\ProfileType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EnumType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfileFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName', TextType::class, ['label' => 'Prénom'])
+            ->add('lastName',  TextType::class, ['label' => 'Nom'])
+            ->add('profileType', EnumType::class, [
+                'class'        => ProfileType::class,
+                'label'        => 'Type de profil',
+                'choice_label' => fn(ProfileType $p) => $p->label(),
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults(['data_class' => UpdateProfileDTO::class]);
+    }
+}
